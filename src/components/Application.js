1dbabd34c1a887b7ee542594e091ed390382ffc8
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2017-06-04 22:42:24 +0600

    React search added for the time zone

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import moment from "moment-timezone";
+import Select from "react-select";
 
 class Application extends Component {
+    constructor () {
+        super();
+        let user_timezone = this.getUserTimeZone();
+        let timezones = moment.tz.names();
+        this.state = {
+            user_timezone,
+            timezones
+        };
+    }
+
+    getUserTimeZone () {
+        return moment.tz.guess();
+    }
+
+    componentWillMount () {
+
+    }
+
     render () {
+        var options = this.state.timezones.map((value) => {
+            return {
+                value: value,
+                label: value
+            }
+        });
         return (
-            <div className = "container-fluid">this is container fluid</div>
+            <div className = "container-fluid">
+                <Select
+                    name = "form-field-name"
+                    value = {this.state.user_timezone}
+                    options = {options}
+                />
+            </div>
         );
     }
 }
